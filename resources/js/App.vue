4d18e9693805ea9e1d43d68a2c88185c924a5e39
nguyen van quang <nguyenvanquang22103@gmail.com>
2024-03-08 17:24:20 +0700

    start project

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="app_vue">
+       
+    </div>
+</template>
---
+++ resources/js/router/index.js
@@ -0,0 +1,18 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+
+
+
+
+export const routes = [
+    {
+
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
